8c9ad3d0a156bdae2bc9d9a9549b360f499bbfee
yousufu3 <opamps4@gmail.com>
2023-08-27 22:46:21 +0000

    Add user micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -54,14 +55,7 @@ class UsersController < ApplicationController
     end
 
     #　beforeフィルタ
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
-    end
+
 
     # 正しいユーザーかどうか確認
     def correct_user

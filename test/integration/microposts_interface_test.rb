f8159edbad9e8340bc6bd4f2923c80d95285df31
yousufu3 <opamps4@gmail.com>
2023-09-06 09:29:49 +0000

    Adduser following

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -25,7 +25,7 @@ class MicropostsInterfaceTest < MicropostsInterface
 
   test "should create a micropost on valid submission" do
     content = "This micropost really ties the room together"
-    assert_difference "Micropost.count", 1 do
+    assert_difference 'Micropost.count', 1 do
       post microposts_path, params: { micropost: { content: content } }
     end
     assert_redirected_to root_url
@@ -35,54 +35,18 @@ class MicropostsInterfaceTest < MicropostsInterface
 
   test "should have micropost delete links on own profile page" do
     get user_path(@user)
-    assert_select "a", text: "delete"
+    assert_select 'a', text: 'delete'
   end
 
   test "should be able to delete own micropost" do
     first_micropost = @user.microposts.paginate(page: 1).first
-    assert_difference "Micropost.count", -1 do
+    assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost)
     end
   end
 
   test "should not have delete links on other user's profile page" do
     get user_path(users(:archer))
-    assert_select "a", { text: "delete", count: 0 }
+    assert_select 'a', { text: 'delete', count: 0 }
   end
 end
-
-class MicropostSidevarTest < MicropostsInterface
-
-  test "should display the right micropost count" do
-    get root_path
-    assert_match "#{@user.microposts.count} microposts", response.body
-  end
-
-  test "should user proper pluralization for zero microposts" do
-    log_in_as(users(:malory))
-    get root_path
-    assert_match "0 microposts", response.body
-  end
-
-  test "should user proper pluralization for one microposts" do
-    log_in_as(users(:lana))
-    get root_path
-    assert_match "1 micropost", response.body
-  end
-end
-
-class ImageUploadTest < MicropostsInterface
-
-  test "should have a file input field for images" do
-    get root_path
-    assert_select "input[type=file]"
-  end
-
-  test "should be able to attach an image" do
-    cont = "This micropost really ties the room together."
-    img = fixture_file_upload("kitten.jpg", "image/jpeg")
-    post microposts_path, params: { micropost: { content: cont, image: img } }
-    assert assigns(:micropost).image.attached?
-  end
-end
-
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -15,23 +15,22 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                               password:               "foo",
                                               password_confirmation:  "bar" } }
     assert_template "users/edit"
-    assert_select "div.alert","The form contains 4 errors"#演習１０．１．３
   end
 
-  test "successgul edit with friendly forwarding" do
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
     log_in_as(@user)
     assert_redirected_to edit_user_url(@user)
-    name = "Foo Bar"
+    name  = "Foo Bar"
     email = "foo@bar.com"
-    patch user_path(@user), params: { user: { name: name,
+    patch user_path(@user), params: { user: { name:  name,
                                               email: email,
-                                              password:       "",
+                                              password:              "",
                                               password_confirmation: "" } }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
-    assert_equal name, @user.name
-    assert_equal email,@user.email
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
   end
 end
